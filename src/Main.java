a104dd110c1855628c67d169d5b84d763c3074c5
Chanel Morgan <chanelmorgan05@icloud.com>
2024-04-08 08:56:25 +0100

    Testing

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,8 @@
 public class Main {
 
     public static void main(String[] args) {
-
-
-
-
         // Testing
         Pizza basePizza = new Pizza(false);
-       // basePizza.getPizzaPrice();
         basePizza.addExtraCheese();
         basePizza.addExtraToppings();
         basePizza.takeAway();
